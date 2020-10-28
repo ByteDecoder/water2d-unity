4a74e4f1ced5436a33b41cbd50e1ea3e31cf1e19
ByteDecoder <rreyes@canaryhealth.com>
2020-10-28 02:45:26 -0600

    localScale added + code refactor

--- Assets/Water2D Surface/scripts/WaterReflectableScript.cs
+++ Assets/Water2D Surface/scripts/WaterReflectableScript.cs
@@ -13,6 +13,8 @@ public class WaterReflectableScript : MonoBehaviour
   [Header("Reflect properties")]
   public Vector3 localPosition = new Vector3(0, -0.25f, 0);
   public Vector3 localRotation = new Vector3(0, 0, -180);
+  public Vector3 localScale = new Vector3(1, 1, 1);
+
   [Tooltip("Optionnal: force the reflected sprite. If null it will be a copy of the source.")]
   public Sprite sprite;
   public string spriteLayer = "Default";
@@ -31,7 +33,7 @@ public class WaterReflectableScript : MonoBehaviour
     reflectGo.transform.parent = this.transform;
     reflectGo.transform.localPosition = localPosition;
     reflectGo.transform.localRotation = Quaternion.Euler(localRotation);
-    reflectGo.transform.localScale = new Vector3(reflectGo.transform.localScale.x, reflectGo.transform.localScale.y, reflectGo.transform.localScale.z);
+    reflectGo.transform.localScale = localScale;
 
     spriteRenderer = reflectGo.AddComponent<SpriteRenderer>();
     spriteRenderer.sortingLayerName = spriteLayer;
@@ -50,20 +52,12 @@ public class WaterReflectableScript : MonoBehaviour
 
   void LateUpdate()
   {
-    if (spriteSource != null)
-    {
-      if (sprite == null)
-      {
-        spriteRenderer.sprite = spriteSource.sprite;
-      }
-      else
-      {
-        spriteRenderer.sprite = sprite;
-      }
-      spriteRenderer.flipX = spriteSource.flipX;
-      spriteRenderer.flipY = spriteSource.flipY;
-      spriteRenderer.color = spriteSource.color;
-    }
+    if (spriteSource == null) return;
+
+    spriteRenderer.sprite = sprite == null ? spriteSource.sprite : sprite;
+    spriteRenderer.flipX = spriteSource.flipX;
+    spriteRenderer.flipY = spriteSource.flipY;
+    spriteRenderer.color = spriteSource.color;
   }
 
   #endregion
